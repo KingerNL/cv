075732994d09707de36ac7c6b67df58df72c623a
Jeramai <j.faber@provrex.nl>
2021-03-17 09:24:38 +0100

    - Updated js to work with latest website

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 (function () {
     //  Change 'Australia' to 'Amsterdam' to test @KingerNL 😉  
     if (Intl.DateTimeFormat().resolvedOptions().timeZone.includes('Australia')) {
+        document.body.style.margin = '0'
+        document.body.style.height = '100vh'
+        document.body.style.overflow = 'hidden'
         document.body.style.transform = 'rotate(180deg)'
     }
 })();
