1109932cc2efce0382a50559d59142926d210720
KingerNL <mart.veldkamp@gmail.com>
2021-04-06 20:26:18 +0200

    pog

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+let isLocalhost = ''
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    isLocalhost = '.html'
+
 function pageSwitch(page){
     $(".loader-wrapper").fadeIn("slow")
-    setTimeout(()=>{window.location = page;},1100)
+    setTimeout(()=>{
+        window.location = page + isLocalhost
+        },1100)
   }
+
+  setTimeout(function(){$(".loader-wrapper").fadeOut("slow")  }, 500)
