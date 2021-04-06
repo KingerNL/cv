64f46f3fb8d03835e76ce82846e16e50f0f7562b
KingerNL <mart.veldkamp@gmail.com>
2021-04-06 20:55:59 +0200

    pog

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function pageSwitch(page){
     $(".loader-wrapper").fadeIn("slow")
     setTimeout(()=>{
         window.location = page + isLocalhost
-        },1100)
+        },600)
   }
 
   setTimeout(function(){$(".loader-wrapper").fadeOut("slow")  }, 500)
